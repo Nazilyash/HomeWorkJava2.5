ad052b71d8739445b07da9f4d8451bd86925ae54
misharka <fareshta88@gmail.com>
2021-01-10 17:23:44 +0300

    cnanged method`s name to findMax

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -7,7 +7,7 @@ public class StatisticsService {
    * @param incomes - array of incomes
    * @return - index of first max value
    */
-  public long FindMax(long[] incomes) {
+  public long findMax(long[] incomes) {
     long current_max_index = 0;
     long current_max = incomes[0];
     for (long income : incomes)
